f7b8549c3e8353b075aa94572a3e68355df8912a
MrIllas <44169046+MrIllas@users.noreply.github.com>
2023-11-17 10:18:34 +0100

    Networkmanager

--- Assets/Scripts/Network/NetworkManager.cs
+++ Assets/Scripts/Network/NetworkManager.cs
@@ -1,8 +1,9 @@
-using System;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 using UnityEngine;
 
@@ -57,4 +58,44 @@ public class NetworkManager : MonoBehaviour
 
     }
     #endregion
+
+    #region Serialize/Deserialize
+
+    /// <summary>
+    /// This function asks for a gameinfo to be serialized and outputs that serialized data converted in the byte array you just passed
+    /// </summary>
+    /// <param name="infoToSend"></param>
+    /// <param name="data"></param>
+    public void Serialize(GameInfo infoToSend, ref byte[] data)
+    {
+        //Convert from our InfoStruct to json
+        string json = JsonUtility.ToJson(infoToSend);
+
+        data = new byte[json.Length];
+        //From Json to byte array
+        data = Encoding.ASCII.GetBytes(json);
+
+    }
+
+    /// <summary>
+    /// Asks for the data byte array and returns the game info with the data 
+    /// </summary>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    public void Deserialize(byte[] data)
+    {
+
+        //Convert from our byte array to string json
+        string json = Encoding.UTF8.GetString(data);
+
+        GameInfo info = JsonUtility.FromJson<GameInfo>(json);
+
+        Debug.Log("JSON despues de la deserializaci�n: " + json);
+
+        //Update GameManager's Data with game info
+        info.SetInfo();
+
+    }
+
+    #endregion Serialize/Deserialize
 }
