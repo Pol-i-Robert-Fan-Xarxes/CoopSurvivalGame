34f8ac2f93b56665569103cd480a4bd19dfb1275
Loproxi <79161178+Loproxi@users.noreply.github.com>
2023-12-19 23:11:43 +0100

    Player attack with flip

--- Assets/Scripts/Entities/Player/PlayerAttack.cs
+++ Assets/Scripts/Entities/Player/PlayerAttack.cs
@@ -6,28 +6,39 @@ using UnityEngine;
 public class PlayerAttack : MonoBehaviour
 {
 
-    private GameObject attackArea;
+    //PLAYER ATTACK RELATED
+    private GameObject attackAreaRight;
+    private GameObject attackAreaLeft;
     private GameObject rightHand;
     [SerializeField] private float attackTimer = 500.0f;
     [SerializeField] private float chargeTimer = 500.0f;
-
     private bool attacking = false;
     private bool charged = false;
     float currentTime = 0.0f;
     float currentChargeTime = 0.0f;
 
+    //LOCAL PLAYER RELATED
+    private SpriteRenderer _playerSprite;
+
     // Start is called before the first frame update
     void Start()
     {
 
-        attackArea = transform.GetChild(3).gameObject;
+        attackAreaRight = transform.GetChild(3).gameObject;
         rightHand = transform.GetChild(4).gameObject;
+        attackAreaLeft = transform.GetChild(5).gameObject;
+
+        _playerSprite = transform.GetComponent<SpriteRenderer>();
+
+        attackAreaLeft.SetActive(false);
+        attackAreaRight.SetActive(false);
 
     }
 
     // Update is called once per frame
     void Update()
     {
+        rightHand.GetComponent<SpriteRenderer>().flipX = _playerSprite.flipX;
 
         ChargeLogic();
 
@@ -48,19 +59,41 @@ public class PlayerAttack : MonoBehaviour
             
             currentTime = 0.0f;
             attacking = false;
-            attackArea.SetActive(false);
+
+            if (_playerSprite.flipX)
+            {
+                attackAreaLeft.SetActive(false);
+                attackAreaRight.SetActive(false);
+            }
+            else
+            {
+                attackAreaRight.SetActive(false);
+                attackAreaLeft.SetActive(false);
+            }
 
         }
         else
         {
-            //Normalize Values
-            float interFactor = currentTime / attackTimer;
-            // Use LerpAngle to interpolate between -90 and 0 degrees
-            float currentChargeNormalized = Mathf.LerpAngle(0.0f, 90.0f, interFactor);
-            rightHand.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentChargeNormalized);
 
             //float currentAttackNormalized = Mathf.Lerp(90.0f, 0.0f, currentTime); // 0-1
             //rightHand.transform.Rotate(Vector3.forward, currentAttackNormalized);
+
+            if (_playerSprite.flipX)
+            {
+                //Normalize Values
+                float interFactor = currentTime / attackTimer;
+                // Use LerpAngle to interpolate between -90 and 0 degrees
+                float currentChargeNormalized = Mathf.LerpAngle(-90.0f, 0.0f, interFactor);
+                rightHand.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentChargeNormalized);
+            }
+            else
+            {
+                //Normalize Values
+                float interFactor = currentTime / attackTimer;
+                // Use LerpAngle to interpolate between -90 and 0 degrees
+                float currentChargeNormalized = Mathf.LerpAngle(90.0f, 0.0f, interFactor);
+                rightHand.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentChargeNormalized);
+            }
         }
     }
 
@@ -80,11 +113,23 @@ public class PlayerAttack : MonoBehaviour
         }
         else
         {
-            //Normalize Value
-            float interFactor = currentChargeTime / chargeTimer;
-            // Use Mathf.LerpAngle to interpolate between 0 and 90 degrees
-            float currentChargeNormalized = Mathf.LerpAngle(0.0f,90.0f, interFactor);
-            rightHand.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentChargeNormalized);
+
+            if(_playerSprite.flipX)
+            {
+                //Normalize Value
+                float interFactor = currentChargeTime / chargeTimer;
+                // Use Mathf.LerpAngle to interpolate between 0 and 90 degrees
+                float currentChargeNormalized = Mathf.LerpAngle(0.0f, -90.0f, interFactor);
+                rightHand.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentChargeNormalized);
+            }
+            else 
+            {
+                //Normalize Value
+                float interFactor = currentChargeTime / chargeTimer;
+                // Use Mathf.LerpAngle to interpolate between 0 and 90 degrees
+                float currentChargeNormalized = Mathf.LerpAngle(0.0f, 90.0f, interFactor);
+                rightHand.transform.rotation = Quaternion.Euler(0.0f, 0.0f, currentChargeNormalized);
+            }
         }
     }
 
@@ -94,7 +139,16 @@ public class PlayerAttack : MonoBehaviour
 
         attacking = true;
         charged = false;
-        attackArea.SetActive(true);
+
+        if (_playerSprite.flipX)
+        {
+            attackAreaLeft.SetActive(true);
+        }
+        else
+        {
+            attackAreaRight.SetActive(true);
+        }
+
         Debug.Log("SLASH!");
 
     }
